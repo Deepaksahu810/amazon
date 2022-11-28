030eb123f1e6e98bfd5642d6f0400082bfd3880b
hp <deepaksahu7201@gmail.com>
2022-11-28 23:11:26 +0530

    adding env configuration for different environments

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,22 +6,29 @@ import { User } from './entities/user'
 import { childrenCategories } from './entities/childrenCategories';
 import { subCategories } from './entities/subCategories';
 import { masterCategories } from './entities/masterCategories';
+import { ConfigModule } from '@nestjs/config';
+import { getEnvPath } from './common/helper/env.helper';
 
+const envFilePath: string = getEnvPath(`src/common/envs`);
 
 @Module({
+ 
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '13.235.68.13',
-      port: 3306,
-      username: 'recruitbaeuser',
-      password: 'jtpdsdsd123',
-      database: 'some',
-      entities: [User,childrenCategories,subCategories,masterCategories],
-      synchronize: true,
-    }),
+    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: '13.235.68.13',
+    //   port: 3306,
+    //   username: 'recruitbaeuser',
+    //   password: 'jtpdsdsd123',
+    //   database: 'some',
+    //   entities: [User,childrenCategories,subCategories,masterCategories],
+    //   synchronize: true,
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+
